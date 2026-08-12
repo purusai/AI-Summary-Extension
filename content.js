ef929cd72535f54e367f053619a10826ef27acaf
Purus <purusottampaswan925@gmail.com>
2026-08-12 23:59:28 +0530

    content.js

--- content.js
+++ content.js
@@ -3,4 +3,13 @@ function getArticleText() {
     if (article) return article.innerText;
 
     const paragraphs = Array.from(document.querySelectorAll("p"));
+    return paragraphs.map(p => p.innerText).join("\n");
 }
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(request.type = "GET_ARTICLE_TEXT") {
+        const text = getArticleText();
+        sendResponse({ text });
+    }
+});
